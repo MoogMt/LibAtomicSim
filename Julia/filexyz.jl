ea6e927fbdc203aa4e48f68b07c9f99541513f23
MoogMt <moog.mathieu@gmail.com>
2020-01-20 18:36:13 +0100

    modified:   filexyz.jl

--- Julia/filexyz.jl
+++ Julia/filexyz.jl
@@ -9,7 +9,7 @@ module filexyz
 # TODO
 # - Support for non AtomList Object
 # - Support for AtomMolList
-# - Add More write functions? 
+# - Add More write functions?
 
 export getNbSteps
 export readFastFile, readStep, readXYZ
@@ -49,148 +49,109 @@ function getNbStep( file_path::T1 ) where { T1 <: AbstractString }
 
   return Int(nb_lines/(nb_atoms+2))
 end
+function getNbStepAtoms( file_path::T1 ) where { T1 <: AbstractString }
+
+  if ! isfile( file_path )
+    print("File TRAJEC.xyz does not exists at ",file_path,"\n")
+    return false
+  end
+
+  nb_lines=0
+  nb_atoms=0
+  file_in = open( file_path )
+  while ! eof(file_in)
+    if nb_step == 0
+      nb_atoms = parse(Float64,split( readline( file_in ) )[1] )
+    else
+      temp = readline( file_in )
+    end
+    nb_lines += 1
+  end
+  close(file_in)
+
+  if nb_lines % (nb_atoms+2) != 0
+    print("File TRAJEC.xyz is corrupted!\n")
+    return false
+  end
+
+  return Int(nb_lines/(nb_atoms+2)), nb_atoms
+end
 #------------------------------------------------------------------------------
 
-# Reading file by not managing datas
-# - requires ability to load both all lines of the file
-#   and the array of position of all the trajectory simultaneously
-#   fastest implementation, but also more memory hungry
-function readFastFile( file::T1 ) where { T1 <: AbstractString }
 
-  # Checking file
-  if ! isfile(file)
-    return sim=Vector{ atom_mod.AtomList }( undef, 0 ), false
+# Reading XYZ files
+#==============================================================================#
+function readFileAtomList( file_path::T1 ) where { T1 <: AbstractString }
+
+  #-----------------------------------------------
+  nb_step = getNbSteps( file_path )
+  if nb_step == false
+    return false
   end
+  #-----------------------------------------------
 
-  #--------------
-  # Reading file
-  #----------------------
-  file2=open(file);
-  lines=readlines(file2);
-  close(file2);
-  #------------------------
-
-  #------------------------
-  # Basic data about files
-  #-----------------------------------------
-  if size(lines)[1] == 0
-    print("File is empty\n")
-    return sim=Vector{ atom_mod.AtomList }( undef, 0 )
-  end
-  nb_atoms=parse(Int64,split(lines[1])[1])
-  if size(lines)[1]/(nb_atoms+2) - trunc(size(lines)[1]/(nb_atoms+2)) > 0.00000000001
-    print("ERROR: Problem inside the file! (number of atoms anounced and given do not match).")
-    return sim=Vector{ atom_mod.AtomList }( undef, 0 )
-  end
-  nb_steps=Int(size(lines)[1]/(nb_atoms+2))
-  #------------------------------------------
-
-  # Reading file into array
+  # Init output structure
   #------------------------------------------------
-  sim=Vector{ atom_mod.AtomList }( undef, nb_steps )
+  traj=Vector{ atom_mod.AtomList }( undef, nb_steps )
+  #-------------------------------------------------
+
+  # Reading
+  #------------------------------------------------------
+  file_in = open( file_path )
   for step=1:nb_steps
-      atom_list = atom_mod.AtomList( nb_atoms )
+      nb_atoms=parse( Int64, split( readline( file_in ) )[1] )
+      temp=readline( file_in )
+      traj[step] = atom_mod.AtomList( nb_atoms )
       for atom=1:nb_atoms
-          line_nb=Int((step-1)*(nb_atoms+2)+atom+2)
-          line_content=split( lines[line_nb] )
-          atom_list.names[atom] = line_content[1]
-          atom_list.index[atom] = atom
-          for pos=1:3
-              atom_list.positions[ atom, pos ] = parse( Float64, line_content[ pos+1 ] )
+          keywords=split( readline(file_in) )
+          traj[step].names[atom] = line_keywords[1]
+          traj[step].index[atom] = atom
+          for i=1:3
+              traj[step].positions[ atom, pos ] = parse( Float64, keywords[ i+1 ] )
           end
       end
-      sim[step]=atom_list
   end
   #------------------------------------------------
 
-  return sim, true
+  return traj
 end
+function readFileAtomList( file_path::T1, stride_::T2 ) where { T1 <: AbstractString, T2 <: Int }
 
-# Read a single step
-function readStep( file_hand::T1 ) where { T1 <: IO }
-  # Get number of atoms
-  line_temp=readline(file_hand)
-  nb_atoms = parse(Int,split(line_temp)[1])
-  # Atoms
-  atoms = AtomList(nb_atoms)
-  # Reading comment line
-  readline(file_hand)
-  # Loop over atoms
-  for i=1:nb_atoms
-    line=split(readline(file_hand))
-    atoms.names[i]=line[1]
-    atoms.index[i]=i
-    for j=1:3
-      atoms.positions[i,j] = parse(Float64,line[j+1])
-    end
+  #-----------------------------------------------
+  nb_step = getNbSteps( file_path )
+  if nb_step == false
+    return false
   end
-  return atoms
+  #-----------------------------------------------
+
+  # Init output structure
+  #------------------------------------------------
+  traj=Vector{ atom_mod.AtomList }( undef, nb_steps )
+  #-------------------------------------------------
+
+  # Reading
+  #------------------------------------------------------
+  file_in = open( file_path )
+  for step=1:nb_steps
+      nb_atoms=parse( Int64, split( readline( file_in ) )[1] )
+      temp=readline( file_in )
+      traj[step] = atom_mod.AtomList( nb_atoms )
+      for atom=1:nb_atoms
+          keywords=split( readline(file_in) )
+          traj[step].names[atom] = line_keywords[1]
+          traj[step].index[atom] = atom
+          for i=1:3
+              traj[step].positions[ atom, pos ] = parse( Float64, keywords[ i+1 ] )
+          end
+      end
+  end
+  #------------------------------------------------
+
+  return traj
 end
+#==============================================================================#
 
-# Advanced the cursor one step forward
-function skipStep( file_hand::T1 ) where { T1 <: IO }
-  # Get number of atoms
-  nb_atoms = parse(Int,split(readline(file_hand))[1])
-  # Reading comment line
-  readline(file_hand)
-  # Loop over atoms
-  for i=1:nb_atoms
-    line=split(readline(file_hand))
-  end
-  return
-end
-
-# Read a whole file with memory management
-function readXYZ( file::T1 ) where { T1 <: AbstractString }
-  nb_steps=getNbSteps(file)
-  file_hand=open(file)
-  atoms_sim=Vector{AtomList}(undef,nb_steps)
-  for i=1:nb_steps
-    atoms_sim[1] = readStep(file_hand)
-  end
-  close(file_hand)
-  return atoms_sim
-end
-
-# Read a file with a given stride
-function readXYZ( file::T1, stride::T2 ) where { T1 <: AbstractString, T2 <: Int }
-  nb_steps=getNbSteps(file)
-  file_hand=open(file)
-  atoms_sim=Vector{AtomList}(Int(trunc(nb_steps/stride)))
-  j=1
-  for i=1:nb_steps
-    if i % stride == 0
-      atoms_sim[j]=readStep(file_hand)
-      j += 1
-    else
-      skipStep(file_hand)
-    end
-  end
-  return atoms_sim
-end
-
-# read a whole file with a stride and starting at a specified steps
-function readXYZ( file::T1, stride::T2 , start_step::T3 ) where { T1 <: AbstractString, T2 <: Int , T3 <: Int }
-  nb_steps=getNbSteps(file)
-  file_hand=open(file)
-  atoms_sim=Vector{AtomList}(Int(trunc((nb_steps-start_step)/stride)))
-  for i=1:start_step
-    readEmpty(file_hand)
-  end
-  j=1
-  for i=start_step:nb_steps-stride
-    if i % stride == 0
-      atoms_sim[j]=readStep(file_hand)
-      j += 1
-    else
-      readEmpty(file_hand)
-    end
-  end
-  return atoms_sim
-end
-#--------------------------------------------------------------------------------
-
-# Writing File To Disk
+# Writing XYZ File To Disk (currently solely from AtomList structures )
 #==============================================================================#
 function writeXYZ( file_handle::T1, atoms::T2 ) where { T1 <: IOStream, T2 <: atom_mod.AtomList }
   nb_atoms=size(atoms.names)[1]
