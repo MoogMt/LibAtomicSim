84feb8b7e14e853107c3bf2446af3dae8e2ebdfc
MoogMt <moog.mathieu@gmail.com>
2020-01-22 15:52:18 +0100

    modified:   Julia/cpmd.jl

--- Julia/cpmd.jl
+++ Julia/cpmd.jl
@@ -4,6 +4,7 @@ using conversion
 using utils
 using filexyz
 using press_stress
+using atom_mod
 
 export readInputTimestep, readIntputStrideStress, readIntputStrideTraj
 export readEnergiesFile, readStress, readTraj
@@ -871,24 +872,28 @@ function buildingDataBase( folder_target::T1, file_stress::T2, file_pressure::T3
     #---------------------------------------------------------------------------
     nb_step_stress = getNbStepStress( file_stress_in )
     if nb_step_stress == false
+        print("Interrupting database construction.\n")
         return false
     end
     nb_step_stress = utils.nbStepStriding( nb_step_stress, n_stress )
     #-------------------------------------------
     nb_step_ftraj, nb_atoms_ftraj = getNbStepAtomsFtraj( file_ftrajectory_in )
     if nb_step_ftraj == false
+        print("Interrupting database construction.\n")
         return false
     end
     nb_step_ftraj = utils.nbStepStriding( nb_step_ftraj, n_ftraj )
     #-------------------------------------------
     nb_step_energy = getNbStepEnergies( file_energy_in )
     if nb_step_energy == false
+        print("Interrupting database construction.\n")
         return false
     end
     nb_step_energy = utils.nbStepStriding( nb_step_energy, n_energy )
     #-------------------------------------------
     nb_step_traj = filexyz.getNbSteps( file_trajec_in )
     if nb_step_traj == false
+        print("Interrupting database construction.\n")
         return false
     end
     nb_step_traj = utils.nbStepStriding( nb_step_traj, n_traj )
@@ -921,6 +926,142 @@ function buildingDataBase( folder_target::T1, file_stress::T2, file_pressure::T3
     temp, epot, etot, msd, comp = readEnergies( file_energy_in, n_energy, nb_ignored, target_length )
     writeEnergies( file_energy, temp, epot, etot, msd, comp )
     #--------------------------------------------------------------------------
+
+    return true
+end
+#-------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
+function writeRelaunchPositions( folder_target::T1, atoms::T2 ) where { T1 <: AbstractString }
+
+    #--------------------------------------------------------
+    species = atom_mod.getSpecies( atoms )
+    nb_element_species = atom_mod.getNbElementSpecies( atoms )
+    start_species = atom_mod.getStartSpecies( atoms )
+    nb_species=size(species)[1]
+    #--------------------------------------------------------
+
+    #--------------------------------------------------------
+    for i_spec=1:nb_species
+        file_out=open( string( folder_in_target, species[i_spec], "_relaunch_positions.dat"), "w" )
+        for atom = start_species[i_spec]:start_species[i_spec]+nb_element_species[i_spec]
+            for i=1:3
+                write( file_out, string( atoms.positions[atom,i]), " " )
+            end
+            write(file_out,"\n")
+        end
+        close( file_out )
+    end
+    #--------------------------------------------------------
+
+    return true
+end
+function writeRelaunchVelocities( folder_target::T1, velocities::Array{T2,2} ) where { T1 <: AbstractString, T2 <: Real }
+
+    #----------------------------------------
+    nb_atoms=size(velocities)[1]
+    #----------------------------------------
+
+    #----------------------------------------
+    file_out = open( string( folder_target, "velocities_restart.dat" ), "w" )
+    for atom = 1:nb_atoms
+        for i=1:3
+            write( file_out, string( velocities[atom,i], " " ) )
+        end
+        write("\n")
+    end
+    close( file_out )
+    #----------------------------------------
+
+    return true
+end
+function writeRelaunchVelocitiesTraj( folder_target::T1, traj::Vector{T2} ) where { T1 <: AbstractString, T2 <: atom_mod.AtomList }
+
+    #----------------------------------------
+    nb_atoms=size(velocities)[1]
+    #----------------------------------------
+
+    #----------------------------------------
+    file_out = open( string( folder_target, "velocities_restart.dat" ), "w" )
+    for atom = 1:nb_atoms
+        for i=1:3
+            write( file_out, string( velocities[atom,i], " " ) )
+        end
+        write("\n")
+    end
+    close( file_out )
+    #----------------------------------------
+
+    return true
+end
+function relaunchRunFtraj( folder_in_target::T1 ) where { T1 <: AbstractString }
+
+    #------------------------------------------------
+    traj = filexyz.readFileAtomList( string( folder_in_target, "TRAJEC.xyz" ) )
+    if traj == false
+        return false
+    end
+    nb_step_traj = size(traj)[1]
+    #------------------------------------------------
+
+    #------------------------------------------------
+    if nb_step_traj != nb_step_ftraj
+        return relaunchRunTrajec( folder_in_target )
+    end
+    #------------------------------------------------
+
+    #------------------------------------------------
+    writeRelaunchPositions( folder_in_target, traj[1] )
+    writeRelaunchVelocitiesTraj( folder_in_target, velocities )
+    #------------------------------------------------
+
+    return true
+end
+function relaunchRunFtraj( folder_in_target::T1 ) where { T1 <: AbstractString }
+
+    #------------------------------------------------
+    positions, velocities, forces = readTraj( string( folder_in_target, "FTRAJECTORY" ) )
+    if positions == false
+        return false
+    end
+    #------------------------------------------------
+
+    #------------------------------------------------
+    nb_step_ftraj = size( positions )[1]
+    positions = positions[nb_step_ftraj,:,:]
+    velocities = velocities[nb_step_ftraj,:,:]
+    forces=[]
+    #------------------------------------------------
+
+    #------------------------------------------------
+    traj = filexyz.readFileAtomList( string( folder_in_target, "TRAJEC.xyz" ) )
+    nb_step_traj = size(traj)[1]
+    #------------------------------------------------
+
+    #------------------------------------------------
+    if nb_step_traj != nb_step_ftraj
+        return relaunchRunTrajec( folder_in_target )
+    end
+    #------------------------------------------------
+
+    #------------------------------------------------
+    traj = traj[1]
+    species = atom_mod.getSpecies( traj )
+    nb_element_species = atom_mod.getNbElementSpecies( traj )
+    start_species = atom_mod.getStartSpecies( traj )
+    for specie in species
+        file_out=open( string( folder_in_target, specie, "_positions.dat"), "w" )
+        for atom = start_species[specie]:start_species[specie]+nb_element_species[specie]
+            for i=1:3
+                write( file_out, string( traj.positions[atom,i]), " " )
+            end
+            write(file_out,"\n")
+        end
+        close( file_out )
+    end
+    #------------------------------------------------
+
+
 
     return true
 end
