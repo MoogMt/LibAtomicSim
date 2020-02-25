ea46e8fb9a6246128b64667dcec60a71fa2238a1
Mathieu Moog <moog.mathieu@gmail.com>
2020-02-25 14:37:51 +0100

    modified:   Julia/contact_matrix.jl

--- Julia/contact_matrix.jl
+++ Julia/contact_matrix.jl
@@ -10,105 +10,109 @@ using graph
 # Building Matrix
 #-------------------------------------------------------------------------------
 function buildMatrix( atoms::T1, cell::T2 ) where { T1 <: atom_mod.AtomList , T2 <: cell_mod.Cell_param }
-  nb_atoms=size(atoms.names)[1]
-  matrix=zeros(nb_atoms,nb_atoms)
-  for i=1:nb_atoms
-    for j=1:nb_atoms
-      dist=cell_mod.distance(atoms,cell,i,j)
-      matrix[i,j]=dist
-      matrix[j,i]=dist
+    nb_atoms=size(atoms.names)[1]
+    matrix=zeros(nb_atoms,nb_atoms)
+    for i=1:nb_atoms
+        for j=1:nb_atoms
+            dist=cell_mod.distance(atoms,cell,i,j)
+            matrix[i,j]=dist
+            matrix[j,i]=dist
+        end
     end
-  end
-  return matrix
+    return matrix
 end
 function buildMatrix( atoms::T1 , cell::T2, cut_off::T3 ) where { T1 <: atom_mod.AtomList , T2 <: cell_mod.Cell_param, T3 <: Real }
-  nb_atoms=size(atoms.names)[1]
-  matrix=zeros(Int,nb_atoms,nb_atoms)
-  for i=1:nb_atoms
-    for j=i+1:nb_atoms
-      if cell_mod.distance(atoms,cell,i,j) <= cut_off
-        matrix[i,j]=1
-        matrix[j,i]=1
-      end
+    nb_atoms=size(atoms.names)[1]
+    matrix=zeros(Int,nb_atoms,nb_atoms)
+    for i=1:nb_atoms
+        for j=i+1:nb_atoms
+            if cell_mod.distance(atoms,cell,i,j) <= cut_off
+                matrix[i,j]=1
+                matrix[j,i]=1
+            end
+        end
     end
-  end
-  return matrix
+    return matrix
 end
 #-------------------------------------------------------------------------------
 
 # Getting bonds
 #-------------------------------------------------------------------------------
 function printBonds( atoms::T1, cell::T2, index::T3 , cut_off::T4 ) where { T1 <: atom_mod.AtomList, T2 <: cell_mod.Cell_param , T3 <: Int , T4 <: Real }
-  nb_atoms=size(atoms.names)[1]
-  for i=1:nb_atoms
-    dist=cell_mod.distance(atoms,cell,index,i)
-    if dist < cut_off
-      print("Bond with ", i, "\n");
+    nb_atoms=size(atoms.names)[1]
+    for i=1:nb_atoms
+        dist=cell_mod.distance(atoms,cell,index,i)
+        if dist < cut_off
+            print("Bond with ", i, "\n");
+        end
     end
-  end
-  return
+    return
 end
 #-------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------
 function readMatrix( input::T1 , nb_atoms::T2) where { T1 <: IO, T2 <: Int }
-  matrix=zeros(nb_atoms,nb_atoms)
-  for i=1:nb_atoms
-    line=split(readline(input))
-    for j=1:nb_atoms
-      matrix[i,j] = parse( Float64, line[j] )
+    matrix=zeros(nb_atoms,nb_atoms)
+    for i=1:nb_atoms
+        line=split(readline(input))
+        for j=1:nb_atoms
+            matrix[i,j] = parse( Float64, line[j] )
+        end
     end
-  end
-  if split(readline(input))[1] == "END"
-    return matrix
-  else
-    print("Problem while reading file")
-    return
-  end
+    if split(readline(input))[1] == "END"
+        return matrix
+    else
+        print("Problem while reading file")
+        return
+    end
 end
 function readMatrix( file::T1 ) where { T1 <: AbstractString }
-  file=open(file)
-  nb_steps=Int(split(readline(file))[1])
-  close(file)
-  return nb_steps
+    file=open(file)
+    nb_steps=Int(split(readline(file))[1])
+    close(file)
+    return nb_steps
 end
 function readMatrix( file::T1, step::T2 ) where { T1<: AbstractString, T2<: Int }
-  file=open(file)
-  line=split(readline(file))
-
-  nb_steps=parse(Int,line[1]);
-  nb_atoms=parse(Int,line[2]);
-  if nb_steps < step
-    return false
-  end
-  for i=1:step
-    for j=1:nb_atoms
-      line=readline(file)
-    end
-  end
-  matrix=zeros(nb_atoms,nb_atoms)
-  for i=1:nb_atoms
+    file=open(file)
     line=split(readline(file))
-    for j=1:nb_atoms
-      matrix[i,j]=parse(Float64,line[j])
+
+    nb_steps=parse(Int,line[1]);
+    nb_atoms=parse(Int,line[2]);
+    if nb_steps < step
+        return false
     end
-  end
-  close(file)
-  return matrix
+    for i=1:step
+        for j=1:nb_atoms
+            line=readline(file)
+        end
+    end
+    matrix=zeros(nb_atoms,nb_atoms)
+    for i=1:nb_atoms
+        line=split(readline(file))
+        for j=1:nb_atoms
+            matrix[i,j]=parse(Float64,line[j])
+        end
+    end
+    close(file)
+    return matrix
 end
 #-------------------------------------------------------------------------------
 
-#--------------------------
-function writeMatrix( file::T1, matrix::Array{T2} ) where { T1 <: IO , T2 <: Real }
+#-------------------------------------------------------------------------------
+function writeStepMatrix( handle_out::T1, matrix::Array{T2,2} ) where { T1 <: IO , T2 <: Real }
     nb_atoms=size(matrix)[1]
-    for i=1:nb_atoms
-        for j=1:nb_atoms
-            write(file, string(matrix[i,j]," ") )
+    for atom1=1:nb_atoms
+        for atom2=1:nb_atoms
+            write( handle_out, string( matrix[atom1,atom2], " " ) )
         end
-        write(file,"\n")
+        write( handle_out , "\n")
     end
     return
 end
-#--------------------------
+function writeMatrix( file_out::T1, matrix::Array{T2,3} ) where { T1 <: AbstractString, T2 <: Real }
+
+end
+#-------------------------------------------------------------------------------
+
 
 end
