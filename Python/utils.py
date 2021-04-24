2ff43c4050af95a6985f7c99dac8e91920f8fce0
MoogMt <moog.mathieu@gmail.com>
2021-04-24 16:39:51 +0200

    Update python

---
+++ Python/utils.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 20 00:46:22 2021
+
+@author: moogm
+"""
+
+#=============================================================================
+def countLines( file_path ):
+    return
